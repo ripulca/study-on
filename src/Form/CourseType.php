f176f5a084aec2af9d837a2c86c5883eae704e60
ripulca <reneitpa@gmail.com>
2023-04-16 23:53:56 +0300

    lil fixes of 2 task

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -17,13 +17,17 @@ class CourseType extends AbstractType
         $builder
             ->add('code', TextType::class, [
                 'label'=>'Код',
+                'required' => true,
+                'constraints' => [new Length(max:255, maxMessage: 'Код не должен превышать {{max}} символов')],
             ])
             ->add('name', TextType::class, [
                 'label'=>'Название',
+                'required' => true,
+                'constraints' => [new Length(max:255, maxMessage: 'Название не должно превышать {{max}} символов')],
             ])
             ->add('description', TextareaType::class, [
                 'label'=>'Описание',
-                'constraints' => [new Length(['max' => 1000])],
+                'constraints' => [new Length(max:1000, maxMessage: 'Описание не должно превышать {{max}} символов')],
             ])
         ;
     }
