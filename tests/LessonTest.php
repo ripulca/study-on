6baef06b524e0767ad424f632348be5b4452007b
ripulca <reneitpa@gmail.com>
2023-04-19 23:12:35 +0300

    working on 4 task

---
+++ tests/LessonTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use App\Tests\AbstractTest;
+
+class LessonTest extends AbstractTest
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
